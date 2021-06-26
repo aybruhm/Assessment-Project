08f44da90c48951ef1230273841a602322894624
Israel Abraham <israelvictory87@gmail.com>
2021-06-26 12:58:19 +0100

    Created a customuser model

--- endpoint/models.py
+++ endpoint/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from .managers import CustomUserManager
 
-# Create your models here.
+
+class User(AbstractBaseUser):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=15)
+    email = models.EmailField(unique=True)
+    avatar = models.ImageField()
+
+    is_active = True
+    is_staff = True
+    is_admin = True
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    def has_module_perms(self, app_label):
+        return self.is_admin
