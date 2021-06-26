eef91394eea8109dbd26ca2f69ae728353a0a03b
Israel Abraham <israelvictory87@gmail.com>
2021-06-26 13:53:03 +0100

    Create url routes for _register, _login, and _logout views

--- endpoint/urls.py
+++ endpoint/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
+from endpoint import views
 
 urlpatterns = [
+    path("register/", views._register, name="register"),
+    path("login/", views._login, name="login"),
+    path("logout/", views._logout, name="logout")
 ]
--- endpoint/views.py
+++ endpoint/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth import login, authenticate, logout
+from endpoint.forms import UserCreateForm, UserLoginForm
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+
+@csrf_exempt
+def _register(request):
+    form = UserCreateForm()
+
+    if request.method == "POST":
+        form = UserCreateForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return HttpResponse("You have successfully registered a user!")
+        return HttpResponse(f"{form.error_messages}")
+
+    return HttpResponse("Register Now...")
+
+
+@csrf_exempt
+def _login(request):
+    form = UserLoginForm()
+
+    if request.method == "POST":
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data.get("email")
+            password = form.cleaned_data.get("password")
+            user = authenticate(
+                request, email=email, password=password
+            )
+            if user is not None:
+                login(request, user)
+                return HttpResponse(f"{email}: you have successfully logged in!")
+        return HttpResponse(f"{form.errors}")
+
+    return HttpResponse("Login Now...")
+
+
+@csrf_exempt
+def _logout(request):
+    logout(request)
+    return HttpResponse("You have successfully logged out!")
