66b93fa0eacc199be0069502f7b77cdd280747be
Israel Abraham <israelvictory87@gmail.com>
2021-06-26 17:27:19 +0100

    Refactored project code

--- endpoint/managers.py
+++ endpoint/managers.py
@@ -6,21 +6,39 @@ from django.db import models
 class CustomUserManager(BaseUserManager):
     """
     Custom user model manager where email is 
-    unique identifiers for authentication instead of usernames
+    unique identifiers for authentication instead of username
     """
-    def create_user(self, email, password, *args, **kwargs):
+
+    def create_user(self, first_name, last_name, phone, email, password, **other_fields):
         if not email:
             raise ValueError(_("The Email must be set"))
+
+        if not phone:
+            raise ValueError(_("The Phone number must be set"))
+
         email = self.normalize_email(email)
-        user = self.model(email=email, **kwargs)
+        user = self.model(
+            first_name=first_name, last_name=last_name,
+            phone=phone, email=email, **other_fields
+        )
         user.set_password(password)
         user.save()
         return user
 
-    def create_superuser(self, email, password):
+    def create_superuser(self, first_name, last_name, phone, email, password, **other_fields):
         """
         Create and save Superuser with the given email and password
         """
-        user = self.create_user(email=email, password=password)
-        user.save(using=self._db)
-        return user 
+
+        other_fields.setdefault('is_staff', True)
+        other_fields.setdefault('is_superuser', True)
+        other_fields.setdefault('is_active', True)
+
+        if other_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must be assigned to is_staff=True.')
+
+        if other_fields.get('is_superuser') is not True:
+            raise ValueError(
+                'Superuser must be assigned to is_superuser=True.')
+
+        return self.create_user(first_name, last_name, phone, email, password, **other_fields)
--- endpoint/models.py
+++ endpoint/models.py
@@ -1,29 +1,21 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from .managers import CustomUserManager
 
 
-class User(AbstractBaseUser):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    phone = models.CharField(max_length=15)
+class User(AbstractBaseUser, PermissionsMixin):
+    first_name = models.CharField(max_length=100, blank=True, null=True)
+    last_name = models.CharField(max_length=100, blank=True, null=True)
+    phone = models.CharField(max_length=15, unique=True)
     email = models.EmailField(unique=True)
     avatar = models.ImageField()
-
-    is_active = True
-    is_staff = True
-    is_admin = True
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
 
     objects = CustomUserManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'phone']
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
-
-    def has_perm(self, perm, obj=None):
-        return self.is_admin
-
-    def has_module_perms(self, app_label):
-        return self.is_admin
